5488dd68780eb9a1979d4a2f1c9c69a53454b5c1
KoinoEngineering <email@koino.engineering>
2021-03-17 08:02:53 +0000

    puppeteer test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import fse from 'fs-extra';
+import puppeteer from 'puppeteer';
+import ReactDOM from "react-dom";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async () => {
+    return puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    }).then(async browser => {
+        return browser
+            .newPage()
+            .then(async (page) => {
+                return page
+                    .evaluate(() => document)
+                    .then(async () => {
+                        const container = document.createElement("div")
+                        document.body.id = "body"
+                        container.id = "root"
+                        document.body.appendChild(container)
+                        ReactDOM.render(<App />, container)
+                        return new XMLSerializer().serializeToString(document)
+                    })
+            })
+            .then(htmlString => {
+                return fse.writeFile(__filename + ".snap.html", htmlString)
+            })
+            .then(() => {
+                return browser.close()
+            });
+    })
 });
